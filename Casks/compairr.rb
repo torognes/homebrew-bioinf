2a42d5b964fb98f70f5bb6718e2a38e040293bd3
Torbjørn Rognes <torognes@ifi.uio.no>
2021-06-09 18:26:08 +0200

    Delete trailing whitespace

--- Casks/compairr.rb
+++ Casks/compairr.rb
@@ -8,11 +8,11 @@ cask "compairr" do
     url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-arm64"
     sha256 "4745615c521f9bc586f7fbbab8b7907e3adbfb2f1167b9dd07047faad32d199d"
   end
-  
+
   name "CompAIRR"
   desc "Compare Adaptive Immune Receptor Repertoires"
   homepage "https://github.com/uio-bmi/compairr"
-  
+
   if Hardware::CPU.intel?
     binary "compairr-#{version}-macos-x86_64", target: "compairr"
   else
