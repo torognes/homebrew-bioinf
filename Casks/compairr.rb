de81e5cb363dec4fc507d2dc929c0c3fc3e58a2b
Torbjørn Rognes <torognes@ifi.uio.no>
2022-12-09 12:45:24 +0100

    Remove blank lines in groups

--- Casks/compairr.rb
+++ Casks/compairr.rb
@@ -5,7 +5,6 @@ cask "compairr" do
     sha256 "a2e1b2c9772ee15ffe0fe8fc1d06b2d6f96439f4db90cb8d33961b7f8127712c"
     url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-x86_64"
   end
-
   on_arm do
     sha256 "57555c7f246a6d33689039e1d629372f661a0e783d6e886c0dc09f7ae2350d3d"
     url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-arm64"
@@ -18,7 +17,6 @@ cask "compairr" do
   on_intel do
     binary "compairr-#{version}-macos-x86_64", target: "compairr"
   end
-
   on_arm do
     binary "compairr-#{version}-macos-arm64", target: "compairr"
   end
