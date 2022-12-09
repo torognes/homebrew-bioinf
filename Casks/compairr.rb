718b9a948adfb7dfc85539fff96a6b1480715ac1
Torbjørn Rognes <torognes@ifi.uio.no>
2022-12-09 12:41:18 +0100

    Use on_intel and on_arm in cask

--- Casks/compairr.rb
+++ Casks/compairr.rb
@@ -1,21 +1,25 @@
 cask "compairr" do
   version "1.9.0"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-x86_64"
+  on_intel do
     sha256 "a2e1b2c9772ee15ffe0fe8fc1d06b2d6f96439f4db90cb8d33961b7f8127712c"
-  else
-    url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-arm64"
+    url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-x86_64"
+  end
+
+  on_arm do
     sha256 "57555c7f246a6d33689039e1d629372f661a0e783d6e886c0dc09f7ae2350d3d"
+    url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-arm64"
   end
 
   name "CompAIRR"
   desc "Compare Adaptive Immune Receptor Repertoires"
   homepage "https://github.com/uio-bmi/compairr"
 
-  if Hardware::CPU.intel?
+  on_intel do
     binary "compairr-#{version}-macos-x86_64", target: "compairr"
-  else
+  end
+
+  on_arm do
     binary "compairr-#{version}-macos-arm64", target: "compairr"
   end
 end
