4b26ace434497baa76e80e14d40ae4c80faa5d12
Torbjørn Rognes <torognes@ifi.uio.no>
2023-02-08 18:17:30 +0100

    Update to CompAIRR version 1.11.0

--- Casks/compairr.rb
+++ Casks/compairr.rb
@@ -1,12 +1,12 @@
 cask "compairr" do
-  version "1.9.0"
+  version "1.11.0"
 
   on_intel do
-    sha256 "a2e1b2c9772ee15ffe0fe8fc1d06b2d6f96439f4db90cb8d33961b7f8127712c"
+    sha256 "520b309b597290b02495019e7cddba8d8e59d3795e8159aa278137372a5720d3"
     url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-x86_64"
   end
   on_arm do
-    sha256 "57555c7f246a6d33689039e1d629372f661a0e783d6e886c0dc09f7ae2350d3d"
+    sha256 "d010d092a44ac4ae591473611173616d65ea80071e75216cdb6babeab04eca5d"
     url "https://github.com/uio-bmi/compairr/releases/download/v#{version}/compairr-#{version}-macos-arm64"
   end
 
--- Formula/compairr.rb
+++ Formula/compairr.rb
@@ -1,13 +1,13 @@
 class Compairr < Formula
   desc "Compare Adaptive Immune Receptor Repertoires"
   homepage "https://github.com/uio-bmi/compairr"
-  url "https://github.com/uio-bmi/compairr/archive/refs/tags/v1.9.0.tar.gz"
-  sha256 "cc4b9acc2ff0fb540cb3f431a622077e790846d06ee069d5b82a83cd5168edad"
+  url "https://github.com/uio-bmi/compairr/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "6f1ef238645a89e8d083e902eb72259983248275c0e8939df31408070dfb90dd"
   license "AGPL-3.0-or-later"
 
   resource("source") do
-    url "https://github.com/uio-bmi/compairr/archive/refs/tags/v1.9.0.tar.gz"
-    sha256 "cc4b9acc2ff0fb540cb3f431a622077e790846d06ee069d5b82a83cd5168edad"
+    url "https://github.com/uio-bmi/compairr/archive/refs/tags/v1.11.0.tar.gz"
+    sha256 "6f1ef238645a89e8d083e902eb72259983248275c0e8939df31408070dfb90dd"
   end
 
   def install
